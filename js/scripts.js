c7f4a062afe25d52cd1a9ee180b2142b4c79321e
Devopsninjaa <126977926+Devopsninjaa@users.noreply.github.com>
2024-03-25 13:17:49 +0700

    Update scripts.js
    MD5 hash updated for 20 & 210424

--- js/scripts.js
+++ js/scripts.js
@@ -214,8 +214,8 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== '20042024' //'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '21042024' //'2ac7f43695eb0479d5846bb38eec59cc') {
+        if (MD5($('#invite_code').val()) !== 'cf08ec7aced6814b1c579fc859392791'
+            && MD5($('#invite_code').val()) !== 'bb3557401d3a929df8a30c23b9fc1c04') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
             $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
